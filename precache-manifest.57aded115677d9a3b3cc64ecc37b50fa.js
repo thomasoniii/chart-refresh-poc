512aaf381ee71920768fac2dc71d919bf8288dbd
Jim Thomason <jim@japanesewartuba.com>
2019-02-07 19:40:40 -0600

    Updates

---
+++ precache-manifest.57aded115677d9a3b3cc64ecc37b50fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fb5926c9bbd6af40af4",
+    "url": "/chart-refresh-poc/static/js/runtime~main.0fb5926c.js"
+  },
+  {
+    "revision": "6b4f646f93194851b2f6",
+    "url": "/chart-refresh-poc/static/js/main.6b4f646f.chunk.js"
+  },
+  {
+    "revision": "7c0b0ebff3adc4273beb",
+    "url": "/chart-refresh-poc/static/js/1.7c0b0ebf.chunk.js"
+  },
+  {
+    "revision": "6b4f646f93194851b2f6",
+    "url": "/chart-refresh-poc/static/css/main.6892b23f.chunk.css"
+  },
+  {
+    "revision": "08f556e32346e098a9536bdcd216567a",
+    "url": "/chart-refresh-poc/index.html"
+  }
+];
